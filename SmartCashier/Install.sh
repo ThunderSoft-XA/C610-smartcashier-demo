6af350993460d228ca93aeaa516c9ffda22b2a68
zhanglei0706 <zhanglei0706@thundersoft.com>
2021-01-14 13:56:55 +0800

    rm-all

--- SmartCashier/Install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-curPath=`pwd`
-mkdir /data/detection/
-cp ${curPath}/test/imagenet_slim_labels.txt /data/detection/
-cp ${curPath}/src/inception_v3.dlc /data/detection/
-
-cp ${curPath}/bin/smartcashier /data/
-chmod 755 /data/smartcashier
